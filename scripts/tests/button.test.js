ff75fe3b22abd1bd82c0a36825ad9879742dea26
AgaToma <aga.tomashevska@gmail.com>
2023-03-19 19:04:51 +0000

    Add html file unpack for tests

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,7 +6,12 @@ const buttonClick = require("../button");
 
 //before each needs to be included when testing dom elements
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>"
+    //fs node module unpacks the whole html file which elements would be tested
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -14,4 +19,7 @@ describe("DOM tests", () => {
     buttonClick();
     expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
    });
+   test("h1 should exist", () => {
+    expect(document.getElementsByTagName("h1").length).toBe(1);
+   })
 })
